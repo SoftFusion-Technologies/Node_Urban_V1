6b2a7797b97953c6ab87db35af3492f6ca8eae1a
Benjamin <goosta19802@gmail.com>
2025-05-31 17:54:23 -0300

    Se agrega campos desde y hasta en los ejercicios de la rutina, para definir una duracion

--- Models/MD_TB_RoutineExercises.js
+++ Models/MD_TB_RoutineExercises.js
@@ -1,10 +1,11 @@
 /*
  * Programador: Benjamin Orellana
- * Fecha Cración: 23 /05 / 2025
- * Versión: 1.0
+ * Fecha Creación: 23 /05 / 2025
+ * Fecha Actualizacion 1 : 31 /05 / 2025 se agrega campo desde y hasta
+ * Versión: 1.1
  *
  * Descripción:
- *Este archivo (MD_TB_RoutineExercises.js) contiene la definición del modelo Sequelize para la tabla routine_exercises.
+ * Este archivo (MD_TB_RoutineExercises.js) contiene la definición del modelo Sequelize para la tabla routine_exercises.
  * Tema: Modelos - RoutineExercises
  * Capa: Backend
  */
@@ -37,6 +38,14 @@ const RoutineExercisesModel = db.define(
       type: DataTypes.INTEGER,
       allowNull: true,
       defaultValue: null
+    },
+    desde: {
+      type: DataTypes.DATEONLY,
+      allowNull: false
+    },
+    hasta: {
+      type: DataTypes.DATEONLY,
+      allowNull: true
     },
     created_at: {
       type: DataTypes.DATE,
